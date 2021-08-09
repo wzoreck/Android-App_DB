747fee324f2a41af30e9f82d2b5196dabe6dd667
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-08-09 11:22:38 -0300

    Codigo de teste para navegabilidade entre fragmentos adicionado

--- app/src/main/java/com/example/app_db/HomeFragment.kt
+++ app/src/main/java/com/example/app_db/HomeFragment.kt
@@ -1,6 +1,19 @@
 package com.example.app_db
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val addNewMovieButton =  getView()?.findViewById<Button>(R.id.btnAddNewMovie)
+        addNewMovieButton?.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToAddEditMovieFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
